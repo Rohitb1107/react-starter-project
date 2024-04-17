627b7f6d795bc1afae59d717549aaee1b41812de
Rohit Bagadi <rohitbagadi@prodios.com>
2024-04-17 14:41:27 +0530

    Created React TypeScript Tailwind CSS Starter!

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="m-auto text-5xl flex flex-col items-center justify-center h-screen">
+      <h1 className="text-red-900">Hello, World!</h1>
+      <div>React TypeScript Tailwind CSS Starter</div>
+    </div>
+  );
+}
+
+export default App;
